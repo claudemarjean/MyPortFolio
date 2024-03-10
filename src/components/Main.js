9bc17ec6ebfb8a62b0eb21bea66e20c67ace947f
claudemarjean <marjean.8888@gmail.com>
2024-03-11 00:39:52 +0300

    🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,11 +2,13 @@ import React from "react";
 import About from "./main/About";
 import Blog from "./main/Blog";
 import Service from "./main/Service";
+import Cursus from "./main/Cursus";
 
 const Main = () =>{
     return(
         <main id="main" >
             <About/>
+            <Cursus/>
             <Service/>
             <Blog/>
         </main>
---
+++ src/components/main/Cursus.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Cursus = () => {
+  return (
+    <section className="mytabs_area p_120">
+        	<div className='container '>
+                dsqfqsdfdqsfq
+            </div>
+    </section>
+  )
+}
+
+export default Cursus
