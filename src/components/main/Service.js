97c1c7b34b53467c5fa80ffb069d86abab73bf8a
claudemarjean <marjean.8888@gmail.com>
2024-06-23 13:41:12 +0300

    maj content BDD

--- src/components/main/Service.js
+++ src/components/main/Service.js
@@ -38,7 +38,7 @@ const Service = () => {
                 <h2 className="s-title">Back-End Web Development</h2>
                 <p className="s-description text-center">
                   
-                  Développeur Web Full Stack expérimenté spécialisé dans le développement Back-End, avec une expertise en PHP, CodeIgniter, Symfony, pour créer des systèmes Back-End robustes pour des applications web fluides. Capable également de concevoir des bases de données efficaces pour assurer des fonctionnalités complètes
+                  Développeur Web Full Stack expérimenté spécialisé dans le développement Back-End, avec une expertise en PHP, CodeIgniter, Symfony, pour créer des systèmes Back-End robustes pour des applications web fluides. 
                 </p>
               </div>
             </div>
@@ -46,13 +46,12 @@ const Service = () => {
           <div className="col-md-4">
             <div className="service-box">
               <div className="service-ico">
-                <span className="ico-circle"><i className="bi bi-camera"></i></span>
+                <span className="ico-circle"><i className="bi bi-database"></i></span>
               </div>
               <div className="service-content">
-                <h2 className="s-title">Photographe</h2>
+                <h2 className="s-title">Concepteur de Bases de Données</h2>
                 <p className="s-description text-center">
-                  
-                  Photographe et vidéaste avec une expertise dans la prise de vue vidéo, le montage, la retouche photo et la création de matériel publicitaire percutant.
+                  Concepteur de bases de données spécialisé dans la modélisation et l'optimisation avec UML et MySQL Workbench. Expertise dans la création de structures de données robustes et évolutives pour diverses applications métiers. 
                 </p>
               </div>
             </div>
